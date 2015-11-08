43c2f9180f607ee875181af44706d87b0dcbd6fb
Brandon Orther <brandon.orther@gmail.com>
2015-11-07 17:09:53 -0800

    Remove maid sample config and updated rules.
     - Updated rules to correctly check files to not be access for 1 week before deleting.
     - Add rule to move Screen Shots to special folder if not accessed
       within the last day.

--- maid/rules.rb
+++ maid/rules.rb
@@ -1,7 +1,15 @@
 Maid.rules do
+  rule 'Misc Screenshots' do
+    dir('~/Desktop/Screen shot *').each do |path|
+      if 1.day.since?(accessed_at(path))
+        move(path, '~/Documents/Misc Screenshots/')
+      end
+    end
+  end
+
   rule 'Remove Desktop files not accessed within the last week' do
     dir('~/Desktop/*').each do |path|
-      if !1.week.since?(accessed_at(path))
+      if 1.week.since?(accessed_at(path))
         trash(path)
       end
     end
@@ -9,7 +17,7 @@ Maid.rules do
 
   rule 'Remove Download files not accessed within the last week' do
     dir('~/Downloads/*').each do |path|
-      if !1.week.since?(accessed_at(path))
+      if 1.week.since?(accessed_at(path))
         trash(path)
       end
     end
