eb539c7a6d69b3a28fb2f8ab48c53ddbcdc8cb20
Brandon Orther <brandon.orther@gmail.com>
2015-11-25 02:35:46 -0800

    Updated maid rules.
     - Added a helper function to check when a file was used or created last.
     - Added note that I'm using made alpha version now
     - Added note with URL to Maid::Tools source file for reference in the
       future.

--- maid/rules.rb
+++ maid/rules.rb
@@ -1,23 +1,39 @@
+# NOTE: These maid rules require ALPHA version of maid: 0.8.0.alpha.4
+
+# The Maid::Tools source is a good reference while writing maid rules:
+#   https://github.com/benjaminoakes/maid/blob/master/lib/maid/tools.rb
+
+# Return the used_at time (Spotlight metadata attribute kMDItemLastUsedDate)
+# if available, otherwise return created_at time (Unix ctime) which is should
+# always be available.
+#
+# ## Example
+#
+#     used_or_created_at("foo.zip") # => Sat Apr 09 10:50:01 -0400 2015
+def used_or_created_at(path)
+  return used_at(path) || created_at(path)
+end
+
 Maid.rules do
   rule 'Misc Screenshots' do
     dir('~/Desktop/Screen shot *').each do |path|
-      if 1.day.since?(accessed_at(path))
+      if 1.day.since?(used_or_created_at(path))
         move(path, '~/Documents/Misc Screenshots/')
       end
     end
   end
 
-  rule 'Remove Desktop files not accessed within the last week' do
+  rule 'Remove Desktop files not accessed within the last 3 days' do
     dir('~/Desktop/*').each do |path|
-      if 1.week.since?(accessed_at(path))
+      if 1.week.since?(used_or_created_at(path))
         trash(path)
       end
     end
   end
 
-  rule 'Remove Download files not accessed within the last week' do
+  rule 'Remove Download files not accessed within the last 3 days' do
     dir('~/Downloads/*').each do |path|
-      if 1.week.since?(accessed_at(path))
+      if 1.week.since?(used_or_created_at(path))
         trash(path)
       end
     end
