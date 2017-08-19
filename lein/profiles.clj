ae3d37d3220771235c758a8ff347341d80c51b66
Brandon Orther <brandon@omt.tech>
2017-08-18 21:36:37 -0700

    Removed spacemacs and lein config.

--- lein/profiles.clj
+++
@@ -1,9 +0,0 @@
-{:user
- {:plugins [[cider/cider-nrepl "0.12.0"]
-            [lein-exec "0.3.5"]
-            [refactor-nrepl "2.2.0"]]
-  :dependencies [[alembic "0.3.2"]
-                 [org.clojure/tools.nrepl "0.2.12"]
-                 [org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.93"]]}}
-
